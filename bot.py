26220a790e194cf33e696648bb924f43043b4a27
boitman-122 <114277379+boitman-122@users.noreply.github.com>
2022-09-26 20:13:00 +0530

    created auto approve bot

---
+++ bot.py
@@ -0,0 +1,88 @@
+import time
+import asyncio
+from os import environ as evn
+from .database import Database
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
+
+
+app = Client(
+    name='requestBot',
+    bot_token=evn.get("BOT_TOKEN"),
+    api_id=evn.get("API_ID"),
+    api_hash=evn.get("API_HASH")
+)
+app.db = Database()
+
+
+@app.on_chat_join_request(filters.group | filters.channel)
+async def autoapprove(c, m):
+    await c.db.add_user(m.from_user.id)
+    try:
+        await c.approve_chat_join_request(m.chat.id, m.from_user.id)
+        button = [[
+            InlineKeyboardButton('JOIN MOVIE CHANNEL', url='https://t.me/+K_OmXxehTJVjMzBl')
+            ],[
+            InlineKeyboardButton('JOIN MOVIE GROUP', url='https://t.me/+iiJvauon_6Q1Njk1')
+        ]]
+        markup = InlineKeyboardMarkup(button)
+        caption = f'Hello {m.from_user.mention()}\nYou Request To Join {m.chat.title} Was Approved.'
+        await c.send_photo(
+            m.from_user.id, 
+            photo='https://telegra.ph/file/2f8ce02ad5148d9e1dbd3.jpg', 
+            caption=caption, 
+            reply_markup=markup
+        )
+    except Exception as e:
+        print('Error:', e)
+
+@app.on_message(filters.command('broadcast') & filters.private & filters.incoming)
+async def broadcast(c, m):
+    if not m.reply_to_message:
+        return await m.reply_text('Reply to a message that i need to broadast.', quote=True)
+
+    msg = m.reply_to_message
+    users = await c.db.get_all_users()
+
+    start = time.time()
+    last_edited = 0
+    failed = 0
+    success = 0
+    total = await c.db.total_users_count()
+    sts = await m.reply_text(f'Board Cast Started\n\nTotal no of users: {total}', quote=True)
+
+
+    async def send_msg(c, user_id):
+        try:
+            return await msg.copy(user_id)
+        except FloodWait as e:
+            await asyncio.sleep(e.value + 1)
+            return await send_msg(c, user_id)
+        except InputUserDeactivated:
+            await c.db.delete_user(user_id)
+            print(f"{user_id} : deactivated")
+        except UserIsBlocked:
+            await c.db.delete_user(user_id)
+            print(f"{user_id} : blocked the bot")
+        except PeerIdInvalid:
+            await c.db.delete_user(user_id)
+            print(f"{user_id} : user id invalid")
+        except Exception as e:
+            print(f"{user_id} : {e}")
+
+    async for user in users:
+        user = await send_msg(c, user_id)
+        if user:
+            success += 1
+        else:
+            failed += 1
+        if last_edited - start == 5:
+            try:
+                await sts.edit(f'Total: {total}\nSuccess: {success}\nFailed: {failed}') 
+                last_edited = time.time()
+            except:
+                pass
+        await asyncio.sleep(0.5)
+
+    await sts.edit(f'**BroadCast Completed:**\n\nTotal: {total}\nSuccess: {success}\nFailed: {failed}')
---
+++ database.py
@@ -0,0 +1,36 @@
+from os import environ
+from motor import motor_asyncio as ma
+
+
+class Database:
+    def __init__(self):
+        self.client = ma.AsyncIOMotorClient(environ.get('DATABASE_URL'))
+        self.db = self.client['AutoRequestBot']
+        self.users = self.db.users
+
+
+    async def is_user_exit(self, user_id):
+        user = await self.users.find_one({'id': user_id})
+        return True if user else False
+    
+
+    async def add_user(self, user_id):
+        if not await self.is_user_exist(user_id):
+            await self.users.insert_one({'id': user_id})
+
+
+    async def get_all_users(self):
+        users = self.users.find({})
+        return users
+
+
+    async def total_users_count(self):
+        count = await self.col.count_documents({})
+        return count
+
+
+    async def delete_user(self, user_id):
+        try:
+            await self.user.delete_many({"id": user_id})
+        except:
+            pass
