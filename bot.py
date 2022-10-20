10c11bf0f9bb0101b36a2e18703b70dfb5aac95d
boitman-122 <114277379+boitman-122@users.noreply.github.com>
2022-10-20 18:00:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -52,11 +52,10 @@ I can approve users in Groups/Channels.Add me to your chat and promote me to adm
             ],[
             InlineKeyboardButton('➰️UPDATES➰️', url='https://t.me/+FCUs6o6FXSZkNjc6')
     ]]
-    markup = InlineKeyboardMarkup(button)
-    await m.reply_text(text, reply_markup=markup)
     await m.reply_photo(
         photo='https://telegra.ph/file/2f8ce02ad5148d9e1dbd3.jpg', 
-        caption='Add caption here',
+        caption=text,
+        reply_markup=InlineKeyboardMarkup(button)
     )
 
 
